50928a760ef8aec4f4195b81280b4ae990c3a138
Zakurivau07 <azamatahiev39@gmail.com>
2026-06-07 16:22:50 +0300

    Use actual freesqldatabase.com MySQL credentials and correct DB defaults

--- config/database.php
+++ config/database.php
@@ -3,14 +3,14 @@
  * Конфигурация базы данных
  */
 
-$driver = getenv('DB_DRIVER') ?: 'pgsql';
+$driver = getenv('DB_DRIVER') ?: 'mysql';
 
 $config = [
     'driver' => $driver,
     'host' => getenv('DB_HOST') ?: 'localhost',
     'port' => getenv('DB_PORT') ?: ($driver === 'pgsql' ? 5432 : 3306),
     'database' => getenv('DB_NAME') ?: 'cms_bd',
-    'username' => getenv('DB_USER') ?: 'postgres',
+    'username' => getenv('DB_USER') ?: 'root',
     'password' => getenv('DB_PASSWORD') ?: '',
 ];
 
